287453ae73bdc03fb62b09f549d7adc774a80fcf
RyanLanz123 <ryanlanz6@gmail.com>
2024-10-11 09:44:59 +0100

    add basic component

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Info />
+      <AddItem />
+      <AddItem />
+      <AddItem />
     </div>
   );
+}
+
+function Info(){
+  return (
+    <div>
+      <h1>Inventory System</h1>
+      <p>Mange your stuff</p>
+      </div>
+  )
+}
+
+function AddItem(){
+  return (
+    <form>
+      <label for="text-form">Type something: </label>
+      <input type ="text" id="text-form" />
+    </form>
+  )
 }
 
 export default App;

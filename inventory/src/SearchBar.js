77f5b381cacd2853693fbf61729d9f18e53e7efc
RyanLanz123 <ryanlanz6@gmail.com>
2024-10-24 13:10:31 +0100

    add onChange event

--- inventory/src/SearchBar.js
+++ inventory/src/SearchBar.js
@@ -12,13 +12,13 @@ function SearchBar(props) {
             <h2>Search For An Item</h2>
             <form>
                 <label for= "name-field">Name:</label>
-                <input id="name-field" type="text" value={name}></input>
+                <input id="name-field" type="text" value={name} onChange={ (e) => setName(e.target.value)} />
                 <label for= "price-field">Max Price:</label>
-                <input id="price-field" type="number" value={price}></input>
+                <input id="price-field" type="number" value={price} onChange={ (e) => setPrice(e.target.value)} />
                 <label for= "type-field">Type:</label>
-                <input id="type-field" type="text" value={type}></input>
+                <input id="type-field" type="text" value={type} onChange={ (e) => setType(e.target.value)} />
                 <label for= "brand-field">Brand:</label>
-                <input id="brand-field" type="text" value={brand}></input>
+                <input id="brand-field" type="text" value={brand} onChange={ (e) => setBrand(e.target.value)} />
                 <button type="button">Search</button>
             </form>
         </div>

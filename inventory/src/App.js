4a984173257159bc8dbe1a46e6a64e59110be5f2
RyanLanz123 <ryanlanz6@gmail.com>
2024-10-26 12:47:11 +0100

    added bootstrap and styled page

--- inventory/src/App.js
+++ inventory/src/App.js
@@ -21,11 +21,16 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <SearchBar updateSearchParams= {updateFilters} />
-      <ItemsDisplay items={data["items"]}/>
-      <AddItem addItem={addItemToData}/>
-
+    <div className="container">
+      <div className="row mt-3">
+        <SearchBar updateSearchParams= {updateFilters} />
+      </div>
+      <div className="row mt-3">
+        <ItemsDisplay items={data["items"]}/>
+      </div>
+      <div className="row mt-3">
+        <AddItem addItem={addItemToData}/>
+      </div>
     </div>
   );
 }
